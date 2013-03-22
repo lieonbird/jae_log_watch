3aeb1f122ad9106445bda641a02aab23406a31ec
lieonbird <lieonbird@sina.com>
2013-03-22 18:20:40 +0800

    modify style

--- lib/nginxlogstatistics.rb
+++ lib/nginxlogstatistics.rb
@@ -9,14 +9,14 @@ require 'sinatra'
 def js_statistics_req(app_id,ts)
   begin
     #init js_db
-    unless $js_db then
-      root=File.absolute_path(File.join(File.dirname(__FILE__), ".."))
+    unless $js_db
+      root=File.absolute_path(File.join(File.dirname(__FILE__), '..'))
       abort("#{root} is not a directory.") unless File.exist?(root)
 
-      cfg_file = File.join(root, "config/redis.yml")
+      cfg_file = File.join(root, 'config/redis.yml')
 
       config = {}
-      File.exist?(cfg_file) && File.open(cfg_file) do |f|
+      File.exist?(cfg_file) && File.open(cfg_file) do | f |
         config.update(YAML.load(f))
       end
 
@@ -48,12 +48,12 @@ def js_statistics_req(app_id,ts)
 
 end
 
-get '/request/:app_id/:timespan' do
-  result_h={"retval"=>0,"ok"=>1}
-  result_h["retval"] = js_statistics_req(params[:app_id],params[:timespan].to_i)
-  if result_h["retval"] == -1 then
-    result_h["ok"] = 0
+get('/request/:app_id/:timespan') {
+  result_h = {'retval'=>0,'ok'=>1}
+  result_h['retval'] = js_statistics_req(params[:app_id], params[:timespan].to_i)
+  if result_h['retval'] == -1
+    result_h['ok'] = 0
     status 500
   end
-  result_h.to_s.gsub(/=>/,':')
-end
+  result_h.to_s.gsub(/=>/, ':')
+}
